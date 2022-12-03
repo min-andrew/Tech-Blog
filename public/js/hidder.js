6d9e0455ca4fb46049e877246ee0f1eead2d84b8
Andrew Min <minandrew0711@gmail.com>
2022-12-03 00:13:45 -0800

    fixed css for login and sign up pages

---
+++ public/js/hidder.js
@@ -0,0 +1,22 @@
+const loginhider = document.querySelector("#loginhidderbtn");
+const signhider = document.querySelector("#signuphidderbtn");
+const logincard = document.querySelector(".logincard");
+const signcard = document.querySelector(".signcard");
+
+
+function hidder() {
+    if (loginhider.value === "login") {
+        logincard.style.display = "none";
+        signcard.style.display = "flex";
+        loginhider.setAttribute("value", "sign");
+    } else if (signhider.value === "sign") {
+        signcard.style.display = "none";
+        logincard.style.display = "flex";
+        loginhider.setAttribute("value", "login");
+    }
+    return
+};
+
+
+loginhider.addEventListener('click', hidder);
+signhider.addEventListener('click', hidder);
